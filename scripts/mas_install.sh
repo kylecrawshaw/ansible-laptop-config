7b4224af0bc9cf7101a86f642bb4c6911160ca1f
Kyle Crawshaw <kyle.crawshaw@gmail.com>
2016-10-31 16:42:48 -0400

    Added mas_install.sh script

---
+++ scripts/mas_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+mas_results=$(mas search "$1" | head -n1 )
+mas_app_name=$(echo "$mas_results" | awk '{print $2}')
+
+if [ "$mas_app_name" = "$1" ]; then
+    mas_app_id=$(echo "$mas_results" | awk '{print $1}')
+    mas install "$mas_app_id"
+    exit 0
+fi
+
+echo "$1 could not be found in the Mac App Store!"
+exit 1
