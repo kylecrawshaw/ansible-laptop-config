ff71c21ce811f74be54c4eecde933cf29fd9afe4
Kyle Crawshaw <kyle.crawshaw@gmail.com>
2016-11-01 17:27:52 -0400

    Updated README and cleaned up bootstrap.sh

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # ansible-laptop-config
 Ansible playbooks for managing my personal laptop config
+
+This has only been tested on macOS 10.12 Sierra, but should function on older version's of macOS. Almost everything for macOS is completely automated.
+
+#### Some of the items to be configured:
+- Install zsh/oh-my-zsh
+- Clone and symlink my personal dotfiles
+- Install homebrew packages
+- Install homebrew cask apps
+- Add preferred apps to dock using dockutil
+- Install preferred Mac App Store apps using (mas-cli)[https://github.com/mas-cli/mas]
+
+## Running on macOS
+1. (optional) Modify the sudoers file to not require a password for the main user. This step makes it so you don't have to enter your sudo password mid run since the playbook takes a long time to run.
+    ```
+    myuseraccount    ALL= (ALL) NOPASSWD: ALL
+    ```
+2. Run `sudo ./bootstrap.sh` to configure everything listed above.
+3. Enter your Apple ID and password when prompted.
+4. Wait a long time for the playbook to complete.
+5. Click on any GUI apps that present a visual installer. Some apps do not offer a command line approach to installation.
+6. Remove the line from *step 1*. It is a security risk to run your system without requiring a password for sudo.
+7. Profit!
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 # Check if the mas-cli exists and look up the latest release to install 
+# mas-cli recommends using brew, but the ansible playbook will install brew for us
 if [ ! -f /usr/local/bin/mas ]; then
+    echo "mas-cli was not found! Looking up latest version from Github releases"
     mas_latest_release=$(curl -s https://api.github.com/repos/mas-cli/mas/releases/latest | python -c "import sys, json; sys.stdout.write(json.load(sys.stdin)['assets'][0]['browser_download_url']);");
-    curl -L $mas_latest_release -o mas.zip;
-    unzip mas.zip -d /usr/local/bin/
+    echo "Downloading and installing mas-cli from Github"
+    curl -sL $mas_latest_release -o mas.zip > /dev/null
+    sudo unzip mas.zip -d /usr/local/bin/ > /dev/null
     rm mas.zip
 fi
 
 if [ ! -d "/Applications/Xcode.app" ]; then
+    echo "Xcode is not currently installed. We need Xcode for git and gcc"
+    echo "Getting ready to install from the Mac App Store using mas-cli"
     signed_in=$([[ $(mas account) == *"Not signed in"* ]] && echo "No" || echo "Yes")
     if [ $signed_in == "No" ]; then
         echo "You are not currently signed in to the Mac App Store. Let's get you signed in!"
@@ -19,14 +24,21 @@ if [ ! -d "/Applications/Xcode.app" ]; then
     /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild -license accept
 fi
 
-[ ! -f /usr/local/bin/ansible ] && sudo easy_install pip
-[ ! -f /usr/local/bin/ansible ] && sudo pip install git+git://github.com/ansible/ansible.git@stable-2.2
+# Newly setup computers often don't have pip install.
+if [ ! -f /usr/local/bin/pip ]; then
+    echo "Missing pip! Installing with easy_install"
+    sudo easy_install pip
+fi
 
+# For now we need to specifically install ansible 2.2 since some of the tasks utilize features only in
+# 2.2 and that is not the current version install by `pip installed ansible`
+if [ ! -f /usr/local/bin/ansible ]; then
+    echo "Installing Ansible 2.2"
+    sudo pip install git+git://github.com/ansible/ansible.git@stable-2.2
+fi
+
+# This will get our current console user regardless of whether the script if run with sudo or not
 logged_in_user=$(python -c "import SystemConfiguration, sys; sys.stdout.write(SystemConfiguration.SCDynamicStoreCopyConsoleUser(None, None, None)[0]);")
-
-# Certain ansible tasks call command line tools that prompt for sudo and 'become' will not help
-#echo "Extending sudo timestamp_timeout to 60 minutes."
-# sudo -u "$logged_in_user" ansible -c "local" localhost -m lineinfile -a "dest=/etc/sudoers line='Defaults:{{ ansible_user }} timestamp_timeout=60' state=present" --become --ask-become-pass
 
 # Install ansible-galaxy roles and run main playbook
 sudo -u "$logged_in_user" ansible-galaxy install -r requirements.yml
